3d6a71e685a5d0ef9bcf717d6cf4c984628feb08
Naoki0509 <70335387+Naoki0509@users.noreply.github.com>
2021-07-12 07:33:09 +0900

    📦 Tailwindcssの追加

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -4,19 +4,21 @@ import Link from "next/link";
 
 export function Header() {
 	return (
-		<header>
-			<ul>
-				<li>
-					<Link href="/">
-						<a>Home</a>
-					</Link>
-				</li>
-				<li>
-					<Link href="/about">
-						<a>About</a>
-					</Link>
-				</li>
-			</ul>
+		<header className={styles.header}>
+			<div className={styles.menuber}>
+				<ul>
+					<li>
+						<Link href="/">
+							<a>Home</a>
+						</Link>
+					</li>
+					<li>
+						<Link href="/about">
+							<a>About</a>
+						</Link>
+					</li>
+				</ul>
+			</div>
 		</header>
 	);
 }
