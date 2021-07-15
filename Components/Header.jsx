c235b56868ec258600195dce235f78f0d479cbb3
Naoki0509 <hmcn3176naoki8086@gmail.com>
2021-07-15 22:41:12 +0900

    💡 tailwind css add code

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -1,24 +1,25 @@
-import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
 
 export function Header() {
 	return (
-		<header className={styles.header}>
-			<div className={styles.menuber}>
-				<ul>
-					<li>
-						<Link href="/">
-							<a>Home</a>
-						</Link>
-					</li>
-					<li>
-						<Link href="/about">
-							<a>About</a>
-						</Link>
-					</li>
-				</ul>
-			</div>
-		</header>
+		<div class="bg-gray-200 w-full p-3 font-serif">
+			<header className={styles.header}>
+				<div>
+					<ul class="inline-flex space-x-4">
+						<li class="flex-1">
+							<Link href="/">
+								<a class="text-4xl">index</a>
+							</Link>
+						</li>
+						<li class="flex-1">
+							<Link href="/about">
+								<a class="text-4xl">about</a>
+							</Link>
+						</li>
+					</ul>
+				</div>
+			</header>
+		</div>
 	);
 }
